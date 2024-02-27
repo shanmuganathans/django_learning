12a4e457ffde1c72af948eda023f7b0111107049
shanmuganathan_suresh <shan.july.1994@gmail.com>
2024-02-27 21:11:22 +0530

    Initial commit

---
+++ django_project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts,
+        'title': 'Zen of Python'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
